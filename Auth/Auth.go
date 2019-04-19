a00489e56ccf24cdae4f90bf7d18bf7ece19335e
Evan Haston <evan.haston@darkmatterct.com>
2019-04-19 10:22:02 -0400

    Adding some trial secirity packages

--- Auth/Auth.go
+++ Auth/Auth.go
@@ -1,3 +1,69 @@
 package Auth
 
+import (
+	"database/sql"
+	"encoding/json"
+	"github.com/spf13/viper"
+	"log"
+	"net/http"
+)
 
+type AuthInfo struct {
+	UserName string
+	APIKey string
+
+}
+
+var apikey string;
+
+func AuthenticatedUser(w http.ResponseWriter, r *http.Request) bool {
+
+
+	var AuthInfo  AuthInfo
+	_ = json.NewDecoder(r.Body).Decode(&AuthInfo)
+	w.Header().Set("Content-Type", "application/json")
+
+	viper.AddConfigPath("/etc/commservice/")
+	viper.SetConfigName("credentials")
+	viper.ReadInConfig()
+	dbusername := viper.GetString("devdb.username")
+	dbpass := viper.GetString("devdb.password")
+	serverip := viper.GetString("devdb.dbhost")
+
+	db, err := sql.Open("mysql", dbusername+":"+dbpass+"@tcp("+serverip+")"+"/")
+	if err != nil {
+		log.Fatal("Sorry there was a problem connecting to the database with user " + dbusername + "Please check /etc/commservice/credentials.yaml")
+
+	}
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer tx.Rollback()
+		stmt, err := tx.Query("SELECT apikey FROM accontrol.tbl_users WHERE username = %v", AuthInfo.UserName )
+		if err != nil {
+			log.Fatal("There was a problem looking you up.")
+		}
+		defer stmt.Close()
+
+
+		for stmt.Next() {
+			err := stmt.Scan(&apikey)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if apikey != AuthInfo.APIKey  {
+				log.Fatal("You are not authenticated")
+				return false
+			}
+
+			if apikey == AuthInfo.APIKey {
+				log.Println("You are authenticated!")
+				return true
+			}
+
+		}
+
+
+	}
